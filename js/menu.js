df2de890e2009756fa561bbfb9a2a44e31b1e207
lucy-lucy <jackiexmelt@gmail.com>
2016-12-22 02:58:01 +0300

    👾  Исправила menu.js согласно критерию Б30

--- js/menu.js
+++ js/menu.js
@@ -6,6 +6,11 @@ mainNav.classList.remove("main-nav--nojs");
 userNav.classList.remove("user-nav--nojs");
 navBtn.classList.remove("page-header__nav-btn--nojs");
 
+// Возможно, это не лучшее решение... Но в 2:57 ночи придумать что-то лучше было сложно :)
+mainNav.classList.add("main-nav--closed");
+userNav.classList.add("user-nav--closed");
+navBtn.classList.add("page-header__nav-btn--closed");
+
 if(navBtn) {
   navBtn.addEventListener("click", function() {
     if(mainNav.classList.contains("main-nav--closed") ||
