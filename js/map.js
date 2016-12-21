b1c28ddcd58e39ed9fac166d4d52446cb829bd01
lucy-lucy <jackiexmelt@gmail.com>
2016-12-21 14:55:50 +0300

    👾  Подключила picturefill

---
+++ js/map.js
@@ -0,0 +1,16 @@
+function initMap() {
+  var mishka = {lat: 59.938750, lng: 30.323178};
+  var mishka_fake = {lat: 59.936319, lng: 30.321681};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: mishka_fake,
+    scrollwheel: false,
+    zoom: 16,
+    disableDefaultUI: true
+  });
+  var image = "img/icon-map-pin.svg";
+  var marker = new google.maps.Marker({
+    position: mishka_fake,
+    map: map,
+    icon: image
+  });
+}
