56605e9e630894497d1946fbcce9e034cb3c4faa
lucy-lucy <jackiexmelt@gmail.com>
2016-12-22 12:25:11 +0300

    👾  Внесла мелкие правки

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 function initMap() {
   var mishka = {lat: 59.938750, lng: 30.323178};
   var mishka_fake = {lat: 59.936319, lng: 30.321681};
-  var map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById("map"), {
     center: mishka_fake,
     scrollwheel: false,
     zoom: 16,
